e7db398a9d8cd34cc1e4c04054ae85fbf0f16078
dwhddnjs <per1215@naver.com>
2022-01-15 03:03:55 +0900

    add email vaild

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
-let header = document.querySelector(".header");
+const header = document.querySelector(".header");
+const subBtn = document.querySelector(".subscribe-btn");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".close-modal");
+
+subBtn.addEventListener("click", openModal);
+closeModal.addEventListener("click", removeModal);
+modal.addEventListener("focusout", removeModal);
+// modal
+function openModal() {
+  const input = document.querySelector(".email");
+
+  if (input.value.length === 0) {
+    alert("이메일 작성해주세요");
+    modal.style.display = "none";
+    return;
+  }
+  if (input.value.length < 11 || !input.value.includes("@")) {
+    alert("이메일을 잘못 입력하셨습니다");
+  } else {
+    modal.style.display = "flex";
+  }
+}
+
+function removeModal() {
+  modal.style.display = "none";
+}
 
 window.onscroll = () => {
   let windowTop = window.scrollY;
