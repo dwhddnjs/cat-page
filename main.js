397ac3d633fda42d972761f34fff5f8108e0e12f
dwhddnjs <per1215@naver.com>
2022-01-20 04:33:17 +0900

    finished renderPage

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ const subBtn = document.querySelector(".subscribe-btn");
 const modal = document.querySelector(".modal");
 const removeModal = document.querySelector(".close-modal");
 const input = document.querySelector(".email");
+const menu = document.querySelector(".menu");
+const pages = document.querySelector(".rendering-page");
+const navbody = document.querySelector(".nav-body");
 
 subBtn.addEventListener("click", openModal);
 removeModal.addEventListener("click", closeModal);
+menu.addEventListener("click", openNav);
 
 function openModal() {
   if (input.value.length === 0) {
@@ -25,10 +29,27 @@ function closeModal() {
   modal.style.display = "none";
 }
 
+function openNav() {
+  pages.style.display = "none";
+  navbody.style.display = "block";
+  const arrow = document.querySelector(".nav-arrow");
+  arrow.addEventListener("click", () => {
+    navbody.style.display = "none";
+    pages.style.display = "block";
+  });
+  const navUl = document.querySelector(".nav-ul");
+  navUl.childNodes.forEach((el) => {
+    el.addEventListener("click", () => {
+      pages.style.display = "block";
+      navbody.style.display = "none";
+    });
+  });
+}
+
 window.onscroll = () => {
   let windowTop = window.scrollY;
 
-  if (windowTop >= 900) {
+  if (windowTop >= 820) {
     header.classList.add("drop");
   } else {
     header.classList.remove("drop");
